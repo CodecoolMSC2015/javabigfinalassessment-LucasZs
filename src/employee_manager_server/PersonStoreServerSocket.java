07faf8463029d5073b0b329312464dcec2a23f03
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-03-16 14:35:54 +0100

    starting to build server socket

--- src/employee_manager_server/PersonStoreServerSocket.java
+++ src/employee_manager_server/PersonStoreServerSocket.java
@@ -1,10 +1,41 @@
 package employee_manager_server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class PersonStoreServerSocket {
 
 	private DataReader store;
 	
+	public PersonStoreServerSocket() throws IOException {
+	
+		ServerSocket ss = new ServerSocket(5678);
+		Socket s = ss.accept();
+		OutputStream os = s.getOutputStream();
+		InputStream is = s.getInputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(os);
+		ObjectInputStream ois = new ObjectInputStream(is);
+	
+	}
+	
 	void start() {
 		
-	};
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			PersonStoreServerSocket psss = new PersonStoreServerSocket();
+			psss.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
